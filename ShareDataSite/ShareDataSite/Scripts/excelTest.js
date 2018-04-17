9f95c0f30e44e9e8c2e963376fb78241ee473ac1
Qiang Han <v-qiahan@microsoft.com>
2018-04-17 15:57:18 +0800

    js comment

--- ShareDataSite/ShareDataSite/Scripts/excelTest.js
+++
@@ -1,7 +0,0 @@
-﻿app.ready(function () {
-    $("#getTable").click(function () {
-        app.getTable(function (result) {
-            $("#log").append(JSON.stringify(result));
-        })
-    })
-})
